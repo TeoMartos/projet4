30592f989f3496b87bab394d5998b77eff6ad8da
TeoMartos <teo.martospro@gmail.com>
2024-12-06 12:13:21 +0100

    [ADD] Gestion du message en fonction de la quantité et de la quantité en stock ok, avec message d'erreur, redirection vers le product line item

--- salesforce_projet_4/force-app/main/default/lwc/getOpportunityProduct/getOpportunityProduct.js
+++ salesforce_projet_4/force-app/main/default/lwc/getOpportunityProduct/getOpportunityProduct.js
@@ -1,58 +1,132 @@
-import { LightningElement, api, wire, track } from 'lwc';
+import {
+    LightningElement,
+    api,
+    wire,
+    track
+} from 'lwc';
+import {
+    NavigationMixin
+} from 'lightning/navigation';
+import {
+    deleteRecord
+} from 'lightning/uiRecordApi';
+
 import getProducts from '@salesforce/apex/ProductController.getProducts';
-import { NavigationMixin } from 'lightning/navigation';
+import getUser from '@salesforce/apex/UserController.getUser';
 
 export default class GetOpportunityProduct extends NavigationMixin(LightningElement) {
     @api recordId;
     @track products;
     @track error;
-    columns = [
-        { label: 'Nom du produit', fieldName: 'productName', type: 'text' },
-        { label: 'Quantité', fieldName: 'Quantity', type: 'currency' },
-        { label: 'Prix Unitaire', fieldName: 'UnitPrice', type: 'currency' },
-        { label: 'Prix Total', fieldName: 'TotalPrice', type: 'currency' },
-        { label: 'Quantité en Stock', fieldName: 'quantityInStock', type: 'number' },
+    @track isButtonDisabled = false;
+    @track hasQuantityIssue = false;
+
+    columns = [{
+            label: 'Nom du produit',
+            fieldName: 'productName',
+            type: 'text'
+        },
         {
-            type: "button", label: 'Delete', initialWidth: 110, typeAttributes: {
-                label: 'Delete',
-                name: 'Delete',
-                title: 'Delete',
-                disabled: false,
-                value: 'delete',
-                iconPosition: 'left',
-                iconName:'utility:delete',
-                variant:'destructive'
+            label: 'Quantité',
+            fieldName: 'Quantity',
+            type: 'number',
+            cellAttributes: {
+                style: {
+                    fieldName: 'quantityStyle'
+                }
             }
         },
         {
-            type: "button", label: 'View', initialWidth: 100, typeAttributes: {
+            label: 'Prix Unitaire',
+            fieldName: 'UnitPrice',
+            type: 'currency'
+        },
+        {
+            label: 'Prix Total',
+            fieldName: 'TotalPrice',
+            type: 'currency'
+        },
+        {
+            label: 'Quantité en Stock',
+            fieldName: 'Quantity_In_Stock__c',
+            type: 'number'
+        },
+        {
+            type: "button",
+            label: 'Delete',
+            initialWidth: 110,
+            typeAttributes: {
+                label: 'Delete',
+                name: 'Delete',
+                title: 'Delete',
+                value: 'delete',
+                iconPosition: 'left',
+                iconName: 'utility:delete',
+                variant: 'destructive'
+            }
+        },
+        {
+            type: "button",
+            label: 'View',
+            initialWidth: 100,
+            typeAttributes: {
                 label: 'View',
                 name: 'View',
                 title: 'View',
-                   disabled: false,
-                        value: 'view',
-                        iconPosition: 'left',
-                        iconName:'utility:preview',
-                        variant:'Brand'
-                }
-           },
+                disabled: false,
+                value: 'view',
+                iconPosition: 'left',
+                iconName: 'utility:preview',
+                variant: 'Brand'
+            }
+        },
     ];
 
+    /*@wire(getUser, {})
+    userData({
+        error,
+        data
+    }) {
+        if (data) {
+            //if(data.Profile.Name === "Commercial") {
+            this.isButtonDisabled = true;
+            console.log('data.Profile.Name === ', data)
+            // }
+        } else if (error) {
+            console.error(error.body.message);
+        }
+    }*/
     wiredProductsResult;
-    @wire(getProducts, { oppId: '$recordId' })
+    @wire(getProducts, {
+        oppId: '$recordId'
+    })
     wiredProducts(result) {
         this.wiredProductsResult = result;
         if (result.data) {
+            this.hasQuantityIssue = false;
             this.products = result.data.map(item => {
-                const transformedItem = {
+                const quantity = item.Quantity || 0;
+                const quantityInStock = item.Quantity_In_Stock__c || 0;
+                if (quantity > quantityInStock) {
+                    this.hasQuantityIssue = true;
+                }
+
+                return {
                     ...item,
                     productName: item.Product2?.Name,
-                    quantityInStock: item.Product2?.QuantityInStock__c || 0,
-                    Product2Id: item.Product2?.Id 
+                    quantityInStock: quantityInStock,
+                    Product2Id: item.Product2?.Id,
+                    OpportunityLineItemId: item.Id,
+                    quantityStyle: quantity > quantityInStock ?
+                        'color: red; font-weight: bold;' :
+                        'color: green; font-weight: bold;'
                 };
-                return transformedItem;
+
             });
-            this.error = this.products.length === 0 ? 'Vous n\'avez pas de produit associées à l\'opportunité' : null;
+
+            this.error = this.products.length === 0 ?
+                'Vous n\'avez pas de produit associées à l\'opportunité' :
+                null;
         } else if (result.error) {
             console.error('Error fetching products:', result.error);
             this.error = 'Une erreur s\'est produite lors du chargement des produits.';
@@ -60,31 +134,48 @@ export default class GetOpportunityProduct extends NavigationMixin(LightningElem
         }
     }
 
-    get errorStyle () {
-        return this.error && this.error.includes('Vous n\'avez aucune ligne de produits pour le moment.\n1. Veuillez tout d\'abord sélectionner un Catalogue (Pricebook) \n2. Sélectionnez ensuite les produits à ajouter.')
-        ? 'slds-notify slds-notify_alert slds-theme_alert-texture slds-theme_info' : 'slds-notify slds-notify_alert slds-theme_alert-texture slds-theme_error'
+    get errorStyle() {
+        return this.error && this.error.includes('Vous n\'avez aucune ligne de produits pour le moment.\n1. Veuillez tout d\'abord sélectionner un Catalogue (Pricebook) \n2. Sélectionnez ensuite les produits à ajouter.') ?
+            'slds-notify slds-notify_alert slds-theme_alert-texture slds-theme_info' : 'slds-notify slds-notify_alert slds-theme_alert-texture slds-theme_error'
     }
 
     callRowAction(event) {
         const actionName = event.detail.action.name;
         const row = event.detail.row;
         if (actionName === 'View') {
-            this.handleSeeProduct(row.Product2Id); 
+            this.handleSeeProduct(row.OpportunityLineItemId);
+        } else if (actionName === 'Delete') {
+            this.handleDeleteProduct(row.Product2Id);
         }
     }
 
     handleSeeProduct(relatedProductId) {
         if (!relatedProductId) {
-            console.error('Product2Id is missing or undefined.');
+            console.log(relatedProductId)
             return;
         }
         this[NavigationMixin.Navigate]({
             type: 'standard__recordPage',
             attributes: {
                 recordId: relatedProductId,
-                objectApiName: 'Product2',
+                objectApiName: 'OpportunityLineItem',
                 actionName: 'view',
             },
         });
+    }
+
+    handleDeleteProduct(relatedProductId) {
+        if (!relatedProductId) {
+            console.error('Product2Id is missing or undefined.');
+            return;
+        }
+        deleteRecord(relatedProductId)
+            .then(result => {
+                console.log('Success delete')
+                return refreshApex(this.wireResult);
+            }).catch(error => {
+                this.error = error;
+                console.log('FAIL delete', error)
+            });
     }
 }
